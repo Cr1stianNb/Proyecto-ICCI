9b323c6a209b774e90aa7ce8704a1b278b3cf925
Cr1stianNb <cristiannettle1@gmail.com>
2023-06-21 22:02:17 -0400

    Organizacion Script C#

--- Proyecto Juego/Assets/Scripts/Script Jefe Worm/JefeIdle.cs
+++ Proyecto Juego/Assets/Scripts/Script Jefe Worm/JefeIdle.cs
@@ -2,30 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JefeAtack : StateMachineBehaviour
+public class JefeIdle : StateMachineBehaviour
 {
-
-    
-
-
-    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       animator.GetComponent<Enemigo>().canHit = false;
+        animator.GetComponent<JefeWorm>().MirarJugador();
+        animator.GetComponent<JefeWorm>().doCoroutine();
     }
 
-    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
-       
-    
+    //    
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-       animator.GetComponent<Enemigo>().canHit = true;
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -38,4 +34,6 @@ public class JefeAtack : StateMachineBehaviour
     //{
     //    // Implement code that sets up animation IK (inverse kinematics)
     //}
+
+   
 }
