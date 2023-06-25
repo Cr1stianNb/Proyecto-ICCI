cb6e77b481ab3984b3d755a25f9d742b495ab0f6
Cr1stianNb <cristiannettle1@gmail.com>
2023-06-25 17:07:42 -0400

    Update Multijugador
    Ahora se pueden pegar

--- Proyecto Juego/Assets/Scripts/Bala.cs
+++ Proyecto Juego/Assets/Scripts/Bala.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Bala : MonoBehaviour
 {
@@ -8,23 +9,19 @@ public class Bala : MonoBehaviour
 
     public float velocidad;
     public float danio;
-  
-    
 
-        
 
-    
+    private Transform owner;
+   
+
+    private void Start()
+    {
+        owner = GetComponentInParent<Transform>(); 
+    }
 
     void Update()
     {
-
-        
-        
-        transform.Translate(Vector2.right * velocidad * Time.deltaTime);
-        
-        
-
-            
+        transform.Translate(Vector2.right * velocidad * Time.deltaTime); 
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -33,6 +30,10 @@ public class Bala : MonoBehaviour
         {
            other.GetComponent<Enemigo>().TomarDanio(danio);
            Destroy(gameObject);
+        }
+        else if(other.CompareTag("Player") && other.gameObject.transform != owner)
+        {
+            other.GetComponent<PlayerHealthController>().DealDamage((int)danio);
         }
 
     }
