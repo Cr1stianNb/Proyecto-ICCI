5aa2df0aff3406cce358f179ac2bdfc209c9147e
Cr1stianNb <cristiannettle1@gmail.com>
2023-07-06 04:22:40 -0400

    Update SistemaPausa.cs

--- Proyecto Juego/Assets/Scripts/GUI Script/SistemaPausa.cs
+++ Proyecto Juego/Assets/Scripts/GUI Script/SistemaPausa.cs
@@ -51,7 +51,7 @@ public class SistemaPausa : MonoBehaviour
     {
         //Destroy(player);
         SalirMenuPausa();
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(51);
         
 
     }
